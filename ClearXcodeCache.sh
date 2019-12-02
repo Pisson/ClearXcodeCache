b400d79b83dfa254892b5defd2affab4dce1cf81
Pisson <554665783@qq.com>
2019-12-02 10:07:06 +0800

    first commit

---
+++ ClearXcodeCache.sh
@@ -0,0 +1,9 @@
+#Clear Xcode cache
+#rm -R /Users/admin/Library/MobileDevice/Provisioning Profiles/*
+cd ~/Library/Developer/Xcode/
+rm -R Products/*
+rm -R Archives/*
+rm -R DerivedData/*
+rm -R UserData/*
+rm -R DocumentationCache/*
+rm -R iOS\ Device\ Logs/*
